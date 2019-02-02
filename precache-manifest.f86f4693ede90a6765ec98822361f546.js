40509059a738a4b961bd27db91c2efcf330e2f80
Gabriel Ferreira <hi.gabrielferreira@gmail.com>
2019-02-01 22:13:43 -0200

    Updates

--- precache-manifest.f86f4693ede90a6765ec98822361f546.js
+++ precache-manifest.f86f4693ede90a6765ec98822361f546.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/itunes/static/js/runtime~main.226c5885.js"
   },
   {
-    "revision": "fe87b058677d50e2987a",
-    "url": "/itunes/static/js/main.fe87b058.chunk.js"
+    "revision": "e27a27985d44eb299c0a",
+    "url": "/itunes/static/js/main.e27a2798.chunk.js"
   },
   {
     "revision": "e1510d61d156f655c7ad",
     "url": "/itunes/static/js/1.e1510d61.chunk.js"
   },
   {
-    "revision": "994e822301d783dca652b98a9b9628f7",
+    "revision": "fee1fa0acbf32c596765a9d56dc669ab",
     "url": "/itunes/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/itunes/precache-manifest.cf7ac35658bead667a0e8ac36bdf9405.js"
+  "/itunes/precache-manifest.f86f4693ede90a6765ec98822361f546.js"
 );
 
 workbox.clientsClaim();
